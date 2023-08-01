ee2ae85cd4c98a5cdc649a001669cd4d2ed5ec98
Denis <denis.boghiu@gmail.com>
2023-08-01 22:58:46 +0300

    interrupt

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ mod app {
         let mut button = gpioe.pe4.into_pull_up_input();
         button.make_interrupt_source(&mut syscfg);
         button.enable_interrupt(&mut ctx.device.EXTI);
-        button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Falling);
+        button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Rising);
         // led
         let led = gpioa.pa6.into_push_pull_output();
 
@@ -62,7 +62,7 @@ mod app {
         }
     }
 
-    #[task(binds = EXTI0, local = [button, led])]
+    #[task(binds = EXTI4, local = [button, led])]
     fn button_click(ctx: button_click::Context) {
         ctx.local.button.clear_interrupt_pending_bit();
         ctx.local.led.toggle();
