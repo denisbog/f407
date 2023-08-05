68e88c7b0227fc9141f766cf106e6f52084392d7
Denis <denis.boghiu@gmail.com>
2023-08-05 12:18:34 +0300

    spi

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,87 @@
-#![deny(unsafe_code)]
-#![deny(warnings)]
 #![no_main]
 #![no_std]
 
 use panic_halt as _;
 
-#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true)]
+#[rtic::app(device = stm32f4xx_hal::pac,  peripherals = true)]
 mod app {
+    use cortex_m::singleton;
     use stm32f4xx_hal::{
-        gpio::{gpioa::PA6, gpioe::PE4, Edge, Input, Output, PushPull},
+        dma::{config::DmaConfig, StreamsTuple},
         prelude::*,
+        spi::{NoMiso, Spi},
     };
-    const SYSFREQ: u32 = 100_000_000;
-    // Shared resources go here
+
     #[shared]
     struct Shared {}
 
-    // Local resources go here
     #[local]
-    struct Local {
-        button: PE4<Input>,
-        led: PA6<Output<PushPull>>,
-    }
+    struct Local {}
 
     #[init]
-    fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
-        // syscfg
-        let mut syscfg = ctx.device.SYSCFG.constrain();
-        // clocks
-        let rcc = ctx.device.RCC.constrain();
-        let _clocks = rcc.cfgr.sysclk(SYSFREQ.Hz()).use_hse(25.MHz()).freeze();
-        // gpio ports A and C
-        let gpioe = ctx.device.GPIOE.split();
-        let gpioa = ctx.device.GPIOA.split();
-        // button
-        let mut button = gpioe.pe4.into_pull_up_input();
-        button.make_interrupt_source(&mut syscfg);
-        button.enable_interrupt(&mut ctx.device.EXTI);
-        button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Rising);
-        // led
-        let led = gpioa.pa6.into_push_pull_output();
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let dp = ctx.device;
 
-        (
-            Shared {
-               // Initialization of shared resources go here
-            },
-            Local {
-                // Initialization of local resources go here
-                button,
-                led,
-            },
-            init::Monotonics(),
-        )
-    }
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.use_hse(25.MHz()).sysclk(80.MHz()).freeze();
 
-    // Optional idle, can be removed if not needed.
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
-        loop {
-            continue;
+        let gpioa = dp.GPIOA.split();
+
+        let mut led = gpioa.pa6.into_push_pull_output();
+        led.set_low();
+
+        let clk = gpioa
+            .pa5
+            .into_alternate()
+            .speed(stm32f4xx_hal::gpio::Speed::VeryHigh)
+            .internal_pull_up(true);
+
+        let mosi = gpioa
+            .pa7
+            .into_alternate()
+            .speed(stm32f4xx_hal::gpio::Speed::VeryHigh);
+
+        let spi = Spi::new(
+            dp.SPI1,
+            (clk, NoMiso::new(), mosi),
+            embedded_hal::spi::MODE_0,
+            3.MHz(),
+            &clocks,
+        );
+
+        let tx = spi.use_dma().tx();
+        let streams = StreamsTuple::new(dp.DMA2);
+        let stream = streams.3;
+
+        let buf = singleton!(: [u8; 100] = [1;100]).unwrap();
+
+        for (i, b) in buf.iter_mut().enumerate() {
+            *b = i as u8;
         }
+
+        let mut transfer = stm32f4xx_hal::dma::Transfer::init_memory_to_peripheral(
+            stream,
+            tx,
+            buf,
+            None,
+            DmaConfig::default()
+                .memory_increment(true)
+                .fifo_enable(true)
+                .fifo_error_interrupt(true)
+                .transfer_complete_interrupt(true),
+        );
+
+        transfer.start(|_tx| {
+            led.set_high();
+        });
+
+        (Shared {}, Local {}, init::Monotonics())
     }
 
-    #[task(binds = EXTI4, local = [button, led])]
-    fn button_click(ctx: button_click::Context) {
-        ctx.local.button.clear_interrupt_pending_bit();
-        ctx.local.led.toggle();
+    #[idle(local = [])]
+    fn idle(_cx: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::nop();
+        }
     }
 }
