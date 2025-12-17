2731b72c90ef276e8134d11392f1c7d5631d43be
Denis <denis.boghiu@gmail.com>
2025-12-17 23:01:29 +0200

    using the socket from the board

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_main]
 #![no_std]
 
+use defmt_rtt as _;
 use panic_rtt_core as _;
 #[rtic::app(device = stm32f4xx_hal::pac)]
 mod app {
@@ -18,24 +19,22 @@ mod app {
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
         // rtt_target::rtt_init_print!();
         rprintln!("start");
+        defmt::println!("led display");
         let dp = ctx.device;
 
         let rcc = dp.RCC.constrain();
-        let clocks = rcc
-            .cfgr
-            //.use_hse(8.MHz()).sysclk(8.MHz())
-            .freeze();
+        let clocks = rcc.cfgr.use_hse(16.MHz()).pclk1(8.MHz()).freeze();
         let gpioa = dp.GPIOA.split();
         let gpiob = dp.GPIOB.split();
         let gpiod = dp.GPIOD.split();
         let gpioe = dp.GPIOE.split();
         let mut delay = dp.TIM1.delay_us(&clocks);
 
-        let lcd = f407::LCD {
-            csx: gpiob.pb12.into_push_pull_output(),
-            dcx: gpiod.pd7.into_push_pull_output(),
-            wrx: gpiod.pd4.into_push_pull_output(),
-            rdx: gpiod.pd5.into_push_pull_output(),
+        let mut lcd = f407::LCD {
+            wrx: gpiod.pd5.into_push_pull_output(),
+            csx: gpiod.pd7.into_push_pull_output(),
+            dcx: gpiod.pd13.into_push_pull_output(),
+            rdx: gpiod.pd4.into_push_pull_output(),
             d0: gpiod.pd14.into_push_pull_output(),
             d1: gpiod.pd15.into_push_pull_output(),
             d2: gpiod.pd0.into_push_pull_output(),
@@ -55,31 +54,36 @@ mod app {
             resx: gpioe.pe4.into_push_pull_output(),
             delay: &mut dp.TIM2.delay_us(&clocks),
         };
-
+        lcd.reset();
         let mut controller = lcd_ili9341::Controller::new(lcd);
         // //reset start
+        //
+        //
+        defmt::println!("reset");
+
+        // //TODO: check what reset is doing
         let mut reset = gpioe.pe3.into_push_pull_output();
 
         reset.set_low();
         delay.delay(10.micros());
         reset.set_high();
         delay.delay(5.millis());
-        // //reset end
+        //reset end
 
-        let mut led = gpioa.pa6.into_push_pull_output();
+        let mut led = gpioa.pa6.into_push_pull_output(); // just for notification, off when writing
+                                                         // to LCD
         led.set_high();
-
         // delay.delay(5.secs());
         delay.delay(5.millis());
         controller.software_reset();
         delay.delay(120.millis());
 
-        // controller.sleep_out();
-        // delay.delay(100.millis());
+        controller.sleep_out();
+        delay.delay(100.millis());
 
-        // controller.display(false);
-        // delay.delay(5.millis());
-        //
+        controller.display(false);
+        delay.delay(5.millis());
+
         // delay.delay(5.secs());
 
         controller.display(true);
@@ -91,16 +95,23 @@ mod app {
         delay.delay(5.millis());
         controller.sleep_out();
         delay.delay(5.millis());
-        controller.column_address_set(0x0000u16, 0x0900u16);
+        controller.column_address_set(0u16, 0u16);
         delay.delay(5.millis());
-        controller.page_address_set(0x0000u16, 0x0009u16);
+        controller.page_address_set(0u16, 0u16);
 
         delay.delay(5.millis());
         controller.memory_write_start();
         delay.delay(5.millis());
-        controller.write_memory(core::iter::repeat(0b1111100000000000).take(240 * 5));
+        controller.write_memory(core::iter::repeat(0b1111110000000000).take(240 * 320));
+
         delay.delay(5.millis());
         led.set_low();
+        delay.delay(50.millis());
+        led.set_high();
+        delay.delay(50.millis());
+        led.set_low();
+
+        defmt::println!("done");
         rprintln!("done");
         (Shared {}, Local {}, init::Monotonics())
     }
